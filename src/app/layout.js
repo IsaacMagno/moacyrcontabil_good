eb1c650960e3918f9d76bcc3d77361f8ca00734f
Gustavo Lemes <gustavolemes997@gmail.com>
2024-03-03 14:33:32 -0300

    Ajusta as fontes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,13 @@
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
 import Navbar from "@/components/layout/MobileNavbar";
 import Footer from "@/components/layout/MobileFooter";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
 
 export const metadata = {
   title: "Moacyr Contabilidade",
@@ -11,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="mx-5">
+      <body className={montserrat.className}>
         <Navbar />
         {children}
         <Footer />
