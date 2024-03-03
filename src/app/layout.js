b73400b395749e14ea72141a5dab75965f685913
Isaac <isaacmagno98@gmail.com>
2024-03-03 17:35:59 -0300

    feat: add contactBar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,12 @@
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-import Navbar from "@/components/layout/MobileNavbar";
-import Footer from "@/components/layout/MobileFooter";
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 import LinkButton from "@/components/layout/LinkButton";
 import { WhatsappIcon } from "@/components/svgs";
 import { Attach } from "@/components/svgs";
+import ContactBar from "@/components/layout/ContactBar";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -21,15 +22,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={montserrat.className}>
+        <div className="hidden lg:block">
+          <ContactBar />
+        </div>
         <Navbar />
         {children}
-        <LinkButton name="Testando um dois tres" />
+        {/* <LinkButton name="Testando um dois tres" />
         <LinkButton
           icon={<WhatsappIcon />}
           wppButton
           name="Testando um dois tres"
         />
-        <LinkButton icon={<Attach />} name="Testando um dois tres" />
+        <LinkButton icon={<Attach />} name="Testando um dois tres" /> */}
         <Footer />
       </body>
     </html>
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -5,9 +5,8 @@ import Link from "next/link";
 import { CSSTransition } from "react-transition-group";
 
 import Logo from "/public/logo_gasparini.png";
-import WhatsAPP from "/public/logo_whatsapp.svg";
 
-import { Menu, X, Instagram, Mail, Phone } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import { navbarButtonList } from "@/helpers/navBarButtonText";
 
 const Navbar = () => {
@@ -18,7 +17,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex flex-col bg-gray-300">
+    <div className="flex flex-col bg-gray-300 px-10">
       <CSSTransition
         in={menuActive}
         timeout={300}
@@ -46,7 +45,9 @@ const Navbar = () => {
             <ul className="flex flex-col min-w-full">
               {navbarButtonList.map((button, index) => (
                 <Link href={button[index][1]} className="mobile-sidebar-link">
-                  <li className="mobile-sidebar">{button[index][0]}</li>
+                  <li className="mobile-sidebar">
+                    <p>{button[index][0]}</p>
+                  </li>
                 </Link>
               ))}
             </ul>
@@ -54,54 +55,14 @@ const Navbar = () => {
         </div>
       </CSSTransition>
 
-      {/* Contacts */}
-      <div className="flex min-w-full justify-between py-2  border-b border-black text-xs lg:text-base items-center">
-        <div className="flex gap-4 ">
-          <a href="mailto:atendimento01@moacyrcontabil.com.br?subject=Contato Moacyr Contabilidade do E-mail&body=Corpo do E-mail">
-            <span className="flex items-center gap-2">
-              <Mail className="navbar-contact-buttons w-4" />
-              <p className="navbar-contact-buttons">Email</p>
-            </span>
-          </a>
-
-          <span className="flex items-center gap-2 ">
-            <Phone className="navbar-contact-buttons w-4" />
-            <a href="tel:+551938932236" className="navbar-contact-buttons">
-              (19) 3893 2236
-            </a>
-          </span>
-        </div>
-        <div className="flex gap-2 items-center">
-          <Link
-            href="https://www.instagram.com/moacyrgasparinicontabilidade/"
-            target="_blank"
-          >
-            <Instagram
-              className="navbar-contact-buttons w-4"
-              strokeWidth={2.8}
-            />
-          </Link>
-          <Link
-            href="https://api.whatsapp.com/send?phone=551938932236&text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20os%20serviços%20da%20contabilidade."
-            target="_blank"
-          >
-            <Image
-              src={WhatsAPP}
-              alt="WhatsApp"
-              className="navbar-contact-buttons w-4"
-            />
-          </Link>
-        </div>
-      </div>
-
       {/* NavBar Buttons */}
       {menuActive ? (
         <div className="min-h-screen bg-black absolute top-0 z-50"> </div>
       ) : null}
-      <div className="flex justify-between py-4 ">
+      <div className="flex justify-between py-4 items-center">
         <Image
           src={Logo}
-          className="w-[7rem] lg:w-[10rem] xl:w-[13rem] 2xl:w-[17rem]"
+          className="w-[7rem] lg:w-[12rem] xl:w-[13rem] 2xl:w-[17rem]"
         />
 
         <div className="block lg:hidden">
@@ -109,11 +70,10 @@ const Navbar = () => {
         </div>
         <ul className="hidden lg:flex gap-4">
           {navbarButtonList.map((button, index) => (
-            <Link
-              href={button[index][1]}
-              className="p-3 hover:opacity-80 2xl:text-xl"
-            >
-              <li className="">{button[index][0]}</li>
+            <Link href={button[index][1]} className="">
+              <li className="hover:opacity-80">
+                <h2>{button[index][0]}</h2>
+              </li>
             </Link>
           ))}
         </ul>
