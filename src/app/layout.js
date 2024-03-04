60cef78e037ac9956d433d8ca8d88084dc780bd4
Gustavo Lemes <gustavolemes997@gmail.com>
2024-03-04 13:08:25 -0300

    adiciona card de informações

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,6 @@
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/layout/MobileNavbar";
-import Footer from "@/components/layout/MobileFooter";
-import LinkButton from "@/components/layout/LinkButton";
-import { WhatsappIcon } from "@/components/svgs";
-import { Attach } from "@/components/svgs";
-import Input from "@/components/layout/Input";
-import Textarea from "@/components/layout/Textarea";
-import Dropdown from "@/components/layout/Dropdown";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -20,13 +13,6 @@ export default function RootLayout({ children }) {
       <body className={montserrat.className}>
         <Navbar />
         {children}
-        <LinkButton name="Padrão" />
-        <LinkButton icon={<WhatsappIcon />} wppButton name="Whatsapp" />
-        <LinkButton icon={<Attach />} name="Anexo" />
-        <Input placeholder="Padrão" type="text" />
-        <Textarea placeholder="Textarea" />
-        <Dropdown placeholder="Dropdown" values={["A", "B", "C"]} />
-        <Footer />
       </body>
     </html>
   );
