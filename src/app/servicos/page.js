12d6b45bb041f94a90eae51b020d49ba3735912a
Gustavo Lemes <gustavolemes997@gmail.com>
2024-03-05 13:18:54 -0300

    adiciona banner na página de serviços

--- src/app/servicos/page.js
+++ src/app/servicos/page.js
@@ -1,9 +1,11 @@
+import BannerServiceSection from "./bannerServiceSection";
 import ContactDoubtSection from "./contactDoubtSection";
 import LocationSection from "./locationSection";
 
 export default function Servicos() {
   return (
     <main className="h-full">
+      <BannerServiceSection />
       <ContactDoubtSection />
       <LocationSection />
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "banner-mobile": "url('../../public/banner mobile.png')",
+        "banner-service": "url('../../public/banner services.png')",
       },
     },
     screens: {
