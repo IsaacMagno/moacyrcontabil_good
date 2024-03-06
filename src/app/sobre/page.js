85ef82d99694772cbc090959b0138c6930e30254
Gustavo Lemes <gustavolemes997@gmail.com>
2024-03-06 13:31:51 -0300

    adiciona banner da página sobre

---
+++ src/app/sobre/page.js
@@ -0,0 +1,9 @@
+import BannerAboutSection from "./bannerAboutSection";
+
+export default function Sobre() {
+  return (
+    <main className="h-full">
+      <BannerAboutSection /> 
+    </main>
+  );
+}
