07aa8e380a5e20b1909161b3521dfb739da0cca8
Isaac <isaacmagno98@gmail.com>
2024-03-12 18:13:09 -0300

    add metadata inicial

--- src/app/inicio/page.js
+++ src/app/inicio/page.js
@@ -2,29 +2,25 @@
 import ServiceSection from "./serviceSection";
 import ContactSection from "../../components/commum-sections/contactSection";
 import DreamSection from "../../components/commum-sections/dreamSection";
+import InfoCard from "../../components/layout/InfoCard";
 import AboutSection from "./aboutSection";
 import HeroSection from "./heroSection";
 import MainSection from "./mainSection";
-import ReactPageScroller from "react-page-scroller";
-import {
-  serviceSectionText,
-  serviceSectionText2,
-} from "@/helpers/servicesSectionText";
-import InfoCard from "../../components/layout/InfoCard";
+import { serviceSectionText } from "@/helpers/servicesSectionText";
+import { metadataInicial } from "@/helpers/metadata";
+
+export const metadata = metadataInicial;
 
 export default function Inicio() {
   return (
-    // <ReactPageScroller>
     <div>
       <HeroSection />
       <InfoCard serviceSectionText={serviceSectionText} />
-      {/* <InfoCard serviceSectionText={serviceSectionText2} /> */}
       <MainSection />
       <ServiceSection />
       <ContactSection />
       <AboutSection />
       <DreamSection />
     </div>
-    // </ReactPageScroller>
   );
 }
--- src/app/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div className="min-h-screen">Resto do site</div>;
-};
-
-export default page;
