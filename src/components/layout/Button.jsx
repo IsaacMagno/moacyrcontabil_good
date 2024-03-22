859c426ae703dabe40943e21bc7ea63a586bf259
Gustavo Lemes <gustavolemes997@gmail.com>
2024-03-22 13:17:20 -0300

    corrige button

--- src/components/layout/Button.jsx
+++ src/components/layout/Button.jsx
@@ -12,11 +12,10 @@ const Button = ({ name, hasBorder, onClick, icon }) => {
       onClick={onClick}
       className={`${
         hasBorder ? "hover:border hover:border-[#F6BE25]" : null
-      } inline-flex justify-center items-center bg-[#F6BE25] border border-transparent rounded-3xl py-2 px-4 gap-2 hover:bg-[#323231] cursor-pointer lmd:p-3 group`}
+      } inline-flex justify-center items-center bg-[#F6BE25] border border-transparent rounded-3xl py-2 px-4 gap-2 hover:bg-[#323231] hover:text-[#F6BE25] cursor-pointer lmd:p-3 group`}
     >
-      <h3 className="flex gap-2 text-[#323231] group-hover:text-[#F6BE25]">
-        {name}{icon ? <div className="w-4 lg:w-6">{icon}</div> : null}
-      </h3>
+      {name}
+      {icon ? <div className="w-4">{icon}</div> : null}
     </button>
   );
 };
